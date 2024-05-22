434fff515ad024f72ee3d4c0ce11e728ec83d559
lucasens1 <lucasensini1@gmail.com>
2024-05-22 15:34:07 +0200

    Milestone 2 with a part of bonus (5)

--- index.php
+++ index.php
@@ -1,22 +1,11 @@
 <?php 
+#Includo il file con le funzioni (Nota bene : Singolo Apice === WARNING a manetta)
+include __DIR__ . "/partials/functions.php";
+
 #Inizializzo una variabile, che contiene l'e-mail, se non è settata la metto vuota
 $mail_address = isset($_POST['mail']) ? $_POST['mail'] : "" ;
 #Console log
-var_dump($mail_address);
-
-#Funzione che controlla sia valida
-function is_valid_address($address){
-    #Da Documentazione filter_var -> filtra una variabile con uno specifico filtro
-    #Caso specifico -> $address è una MAIL valida? (Standard RFC)
-    return filter_var($address, FILTER_VALIDATE_EMAIL);
-}
-
-#Controllo che stampa in pagina i due output
-if(is_valid_address($mail_address)){
-    echo "L'indirizzo inserito è valido";
-}else{
-    echo "L'indirizzo inserito non è valido";
-}
+/* var_dump($mail_address); */
 
 ?>
 
@@ -32,14 +21,16 @@ if(is_valid_address($mail_address)){
 <body>
     <div class="container">
         <div class="row text-center p-4 border border-rounded my-5">
-            <h1 class="p-2">Form di Registrazione : </h1>
+            <h1 class="p-2">Form di Registrazione mail : </h1>
             <div class="col justify-content-center my-4">
                 <form action="index.php" method="POST" class="text-center d-flex justify-content-center align-items-center">
                     <label for="mail" class="mx-2">La tua e-mail : </label>
-                    <input type="text" id="mail" name="mail">
+                    <input type="text" id="mail" name="mail" value="<?php echo isset($_POST['mail']) ? $_POST['mail'] : "" ?>">
                     <button type="submit" class="btn btn-primary mx-2">Invia</button>
                 </form>
             </div>
+            <!-- Richiamo funzione in_page_address da functions.php -->
+            <p><?php in_page_address($mail_address) ?></p>
         </div>
     </div>
 </body>
---
+++ partials/functions.php
@@ -0,0 +1,17 @@
+<?php
+#Funzione che controlla sia valida
+function is_valid_address($check_address){
+    #Da Documentazione filter_var -> filtra una variabile con uno specifico filtro
+    #Caso specifico -> $address è una MAIL valida? (Standard RFC)
+    return filter_var($check_address, FILTER_VALIDATE_EMAIL);
+}
+
+function in_page_address($mail){
+    #Controllo che stampa in pagina i due output
+    if(is_valid_address($mail)){
+        echo "L'indirizzo inserito è valido";
+    }else{
+        echo "L'indirizzo inserito non è valido";
+    }
+}
+?>
