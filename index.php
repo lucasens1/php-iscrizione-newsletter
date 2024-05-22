7b6c76fb83e2f2da20026964119e74cee72c8b85
lucasens1 <lucasensini1@gmail.com>
2024-05-22 16:45:25 +0200

    Bonuses ok

--- index.php
+++ index.php
@@ -1,11 +1,17 @@
 <?php 
+#Apro la sessione
+if(!isset($_SESSION)){
+	session_start();
+}
+
 #Includo il file con le funzioni (Nota bene : Singolo Apice === WARNING a manetta)
-include __DIR__ . "/partials/functions.php";
+include_once __DIR__ . "/partials/functions.php";
 
 #Inizializzo una variabile, che contiene l'e-mail, se non è settata la metto vuota
 $mail_address = isset($_POST['mail']) ? $_POST['mail'] : "" ;
 #Console log
 /* var_dump($mail_address); */
+/* var_dump($valid_flag); */
 
 ?>
 
@@ -23,10 +29,13 @@ $mail_address = isset($_POST['mail']) ? $_POST['mail'] : "" ;
         <div class="row text-center p-4 border border-rounded my-5">
             <h1 class="p-2">Form di Registrazione mail : </h1>
             <div class="col justify-content-center my-4">
-                <form action="index.php" method="POST" class="text-center d-flex justify-content-center align-items-center">
-                    <label for="mail" class="mx-2">La tua e-mail : </label>
-                    <input type="text" id="mail" name="mail" value="<?php echo isset($_POST['mail']) ? $_POST['mail'] : "" ?>">
-                    <button type="submit" class="btn btn-primary mx-2">Invia</button>
+                <form 
+                    action="index.php" 
+                    method="POST" 
+                    class="text-center d-flex justify-content-center align-items-center">
+                        <label for="mail" class="mx-2">La tua e-mail : </label>
+                        <input type="text" id="mail" name="mail" value="<?php echo isset($_POST['mail']) ? $_POST['mail'] : "" ?>">
+                        <button type="submit" class="btn btn-primary mx-2">Invia</button>
                 </form>
             </div>
             <!-- Richiamo funzione in_page_address da functions.php -->
--- partials/functions.php
+++ partials/functions.php
@@ -9,9 +9,11 @@ function is_valid_address($check_address){
 function in_page_address($mail){
     #Controllo che stampa in pagina i due output
     if(is_valid_address($mail)){
-        echo "L'indirizzo inserito è valido";
+        $_SESSION['auth'] = true;
+        header("Location: ./thankyou.php");
     }else{
         echo "L'indirizzo inserito non è valido";
+        $_SESSION['auth'] = false;
     }
 }
 
@@ -22,4 +24,13 @@ function is_successful($mail_color){
         echo "alert-danger";
     }
 }
+
+#Controlla che l'utente abbia i permessi
+function check_auth() {
+    if (!isset($_SESSION["auth"]) || $_SESSION["auth"] !== true) {
+      header("Location: ./index.php");
+      die();
+    }
+}
+
 ?>
